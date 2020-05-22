8f346142a89570cbf7f88133f426b019ef176741
DwarfPorter <mrvlmor@gmail.com>
2020-05-22 20:47:52 +0300

    обработка вычислений в потоке

--- Thread/app/src/main/java/ru/geekbrains/thread/MainActivity.java
+++ Thread/app/src/main/java/ru/geekbrains/thread/MainActivity.java
@@ -13,15 +13,30 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int counterThread = 0;
+    private TextView textView;
+    private TextView textIndicator;
+    private EditText seconds;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
+        initUIButton();
+        initThreadButton();
+    }
+
+    private void initView() {
+        textView = findViewById(R.id.textView);
+        textIndicator = findViewById(R.id.textIndicator);
+        seconds = findViewById(R.id.editText);
+    }
+
+    private void initUIButton() {
         Button button = findViewById(R.id.button);
-        final TextView textView = findViewById(R.id.textView);
-        final TextView textIndicator = findViewById(R.id.textIndicator);
-        final EditText seconds = findViewById(R.id.editText);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,6 +44,35 @@ public class MainActivity extends AppCompatActivity {
                 String result = Long.toString(calculate(sec));
                 textIndicator.setText("В главном потоке");
                 textView.setText(result);
+            }
+        });
+    }
+
+    private void initThreadButton(){
+        Button button = findViewById(R.id.calcThreadBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counterThread++;
+                final int numberThread = counterThread;
+                final int secs = Integer.parseInt(seconds.getText().toString());
+                textIndicator.setText(String.format("%sСтартуем поток %d сек %d\n",
+                        textIndicator.getText().toString(), numberThread, secs));
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        int sec = Integer.parseInt(seconds.getText().toString());
+                        final String result = Long.toString(calculate(sec));
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                textIndicator.setText(String.format("%sИз потока %d\n", textIndicator.getText().toString(),
+                                        numberThread));
+                                textView.setText(result);
+                            }
+                        });
+                    }
+                }).start();
             }
         });
     }
