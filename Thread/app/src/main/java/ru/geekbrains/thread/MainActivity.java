637142376e58bf08f1bcfd3f5326bd7c34edf0a1
DwarfPorter <mrvlmor@gmail.com>
2020-05-22 20:24:29 +0300

    обработка вычислений в главном потоке

---
+++ Thread/app/src/main/java/ru/geekbrains/thread/MainActivity.java
@@ -0,0 +1,46 @@
+package ru.geekbrains.thread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        final TextView textView = findViewById(R.id.textView);
+        final TextView textIndicator = findViewById(R.id.textIndicator);
+        final EditText seconds = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int sec = Integer.parseInt(seconds.getText().toString());
+                String result = Long.toString(calculate(sec));
+                textIndicator.setText("В главном потоке");
+                textView.setText(result);
+            }
+        });
+    }
+
+    private long calculate(int seconds) {
+        Date date = new Date();
+        long diffInSec;
+        do{
+            Date currentDate = new Date();
+            long diffInMs = currentDate.getTime() - date.getTime();
+            diffInSec = TimeUnit.MILLISECONDS.toSeconds(diffInMs);
+        }while(diffInSec < seconds);
+        return diffInSec;
+    }
+}
